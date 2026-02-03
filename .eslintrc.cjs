7e75ae4d9728df1f2c313d4a1e63c0890c3de245
krisballew <131904943+krisballew@users.noreply.github.com>
2026-02-03 05:43:16 +0000

    fix: disable import/no-named-as-default-member rule for express
    Resolves warning for express.json() usage

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,6 +35,7 @@ module.exports = {
         alphabetize: { order: 'asc', caseInsensitive: true },
       },
     ],
+    'import/no-named-as-default-member': 'off',
   },
   settings: {
     'import/resolver': {
